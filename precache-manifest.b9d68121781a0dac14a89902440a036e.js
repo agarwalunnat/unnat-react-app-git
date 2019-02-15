2f1591d620489f4ee2b1d2fc1631257e99725d63
Unnat Agarwal <unnat.agarwal@dporganizer.com>
2019-02-15 09:58:17 +0530

    Updates

---
+++ precache-manifest.b9d68121781a0dac14a89902440a036e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f9c9a3c777bdcd50626e",
+    "url": "/unnat-react-app-git/static/css/main.cbedbf7c.chunk.css"
+  },
+  {
+    "revision": "f9c9a3c777bdcd50626e",
+    "url": "/unnat-react-app-git/static/js/main.f9c9a3c7.chunk.js"
+  },
+  {
+    "revision": "d035b49954cbab025ae2",
+    "url": "/unnat-react-app-git/static/js/runtime~main.d035b499.js"
+  },
+  {
+    "revision": "ba1ca5ba3f9c1e3c91ad",
+    "url": "/unnat-react-app-git/static/js/2.ba1ca5ba.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/unnat-react-app-git/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "8fb1d29837bcc964f635b258d416e018",
+    "url": "/unnat-react-app-git/index.html"
+  }
+];
